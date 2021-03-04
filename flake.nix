62c9bda77d1690a5b2824a764cb32779974c090d
GTrunSec <gtrunsec@hardenedlinux.org>
2021-03-04 01:19:26 -0800

    first commits

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    nixpkgs = { url = "nixpkgs/7d71001b796340b219d1bfa8552c81995017544a"; };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShell = import ./shell.nix { inherit nixpkgs; };
+        }
+      )
+    );
+}
---
+++ shell.nix
@@ -0,0 +1,44 @@
+{ pkgs ? import <nixpkgs> { }
+, system ? builtins.currentSystem
+, ...
+}:
+let
+  fetch = import ./compat/fetch.nix;
+  devshell' = fetch "devshell";
+  devshell = import devshell' {
+    inherit system;
+  };
+in
+devshell.mkShell rec {
+  name = "my-julia2nix-env";
+  packages = with pkgs;
+    [ ];
+
+  env = [
+    {
+      name = "JULIA_DEPOT_PATH";
+      value = "./.julia_depot";
+    }
+    {
+      name = "PATH";
+      prefix = "bin";
+    }
+  ];
+  commands = with pkgs; [
+    {
+      name = "julia2nix";
+      command = ''
+        if [ ! -d  "./julia2nix" ]; then
+        ${pkgs.git}/bin/git clone https://github.com/thomasjm/julia2nix.git
+        fi
+        julia2nix/julia2nix
+      '';
+      category = "julia2nix";
+    }
+    {
+      name = "julia";
+      command = "$(nix-build . --no-out-link)/bin/julia";
+      category = "julia";
+    }
+  ];
+}
