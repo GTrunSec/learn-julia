c48cc851c8c5ea722dd77a809aa1433a511f33ab
GTrunSec <gtrunsec@hardenedlinux.org>
2021-03-04 21:56:41 -0800

    test GR DONE

---
+++ pluto-notebooks/gr.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 24734878-7d42-11eb-10e7-35421889faaf
+using GR
+
+# ╔═╡ 167d66f2-7d42-11eb-1967-3fff5fa301dd
+histogram(randn(10000))
+
+# ╔═╡ Cell order:
+# ╠═24734878-7d42-11eb-10e7-35421889faaf
+# ╠═167d66f2-7d42-11eb-1967-3fff5fa301dd
